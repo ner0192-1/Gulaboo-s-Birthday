27b73e0765ab1652d0cea2f3d0992e23740d5180
ner0192 <0192nike@gmail.com>
2026-07-08 00:10:34 +0530

    Add gh-pages deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
+  base: '/Gulaboo-s-Birthday/',
   plugins: [
     react(),
     imagetools({
